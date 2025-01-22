639fb9b8bb9084c6506dd653b164197625e8cd81
encoder-shaoib <ahammed15-4955@diu.edu.bd>
2025-01-22 20:35:06 +0600

    Basic setup for all freamWork

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div className='flex justify-center '>
+            <h1>I am from root!!!</h1>
+        </div>
+    );
+};
+
+export default Root;
