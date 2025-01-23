de013e6c198dfe3b10cb0983e11b82d3db1f91af
encoder-shaoib <ahammed15-4955@diu.edu.bd>
2025-01-23 21:14:37 +0600

    customize for better locking

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,16 +3,16 @@ import bannerImg from "../../assets/image/books.jpg"
 
 const Banner = () => {
   return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="my-6">
+      <div className="hero min-h-[56.86vh] rounded-xl bg-violet-600 ">
+        <div className="hero-content flex-col lg:flex-row-reverse ">
           <img
             src={bannerImg}
-            className="max-w-sm rounded-lg shadow-2xl"
+            className="max-w-sm rounded-lg shadow-2xl md:w-[900px] md:h-[350px]"
           />
-          <div>
+          <div className="w-3/5 ">
             <h1 className="text-5xl font-bold">Books to freshen up your bookshelf </h1>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn bg-[#23be0a] mt-7 text-white p-4">Get Started</button>
           </div>
         </div>
       </div>
